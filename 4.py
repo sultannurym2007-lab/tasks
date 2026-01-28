bec49226812b31ac93ca25dde12ccc3cdafddeb6
sultannurym2007-lab <sultannurym2007@gmail.com>
2026-01-28 21:55:41 +0600

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,17 @@
+x = 2
+y = "Sultan"
+print(x)
+print(y)
+x = str(2)    # x will be '2'
+y = int(2)    # y will be 2
+z = float(2)  # z will be 2.0
+x = 3
+y = "John"
+print(type(x))
+print(type(y))
+x = "John"
+# is the same as
+x = 'John'
+a = 4
+A = "Sally"
+#A will not overwrite a
